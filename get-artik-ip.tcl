b7e64a4e95e4de70b1fc54bbe7f10ee2e5cddcb7
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-08-22 20:13:29 +0300

    Improve Tcl/Expect script
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

--- get-artik-ip.tcl
+++ get-artik-ip.tcl
@@ -1,50 +1,40 @@
 #!/usr/bin/env expect
-set device [lindex $argv 0]
+#set device [lindex $argv 0]
+set device "/dev/ttyUSB0"
 set baud 115200
 set opts "cs8 ixoff"
 
 set user "root"
 set pass "tizen"
 
-set prompt "(%|#|\\$|root)$"
+set prompt "(%|#|\\$|root:~>)$"
+
+set str {}
 
 log_user 0
 
-spawn sudo screen $device $baud $opts
-send "\r"
+spawn screen -S artik $device $baud $opts
 
-expect {
-    "login:" {
-        send "$user\r"
-        expect "Password:"
-        send "$pass"
+send -- "\r"
+expect -re ".*artik login:" {
+    send -- "${user}\r"
+}
+expect "Password:" {
+    send -- "${pass}\r"
+}
+
+expect "root:~>" {
+    send -- "ip -f inet addr show\r"
+}
+
+expect "root:~>" {
+    set matchTuples [regexp -all -inline {\d+\:\s(\w+)[^\n]*\n\s*inet\s*(\d+\.\d+\.\d+\.\d+)\/(\d+)[^\n]*} $expect_out(buffer)]
+    foreach {group0 group1 group2 group3} $matchTuples {
+        set str "${str}${group1} ${group2} ${group3}\n"
     }
-    -re $prompt
 }
-spawn ip -f inet addr show
-set accum {}
-#expect {
-#    -re {(\d+\: +(\w+).*?\s*inet\s*(\d+\.\d+\.\d+\.\d+)\/(\d+)(?!\d+\:).*\n)+} {
-#        set accum "${accum}$expect_out(2,string) "
-#        set accum "${accum}$expect_out(3,string) "
-#        set accum "${accum}$expect_out(4,string) "
-#        set accum "${accum}\n"
-#        exp_continue
-#    }
-#}
-set tmp {}
-set if {}
-set ip {}
-set port {}
-set matchStr {}
-expect "!\r"
-while {[regexp {\d+\: +(\w+).*?\s*inet\s*(\d+\.\d+\#.\d+\.\d+)\/(\d+)(?!\d+\:)[.\n]*} $expect_out(buffer) -> if ip port]} {
-    set matchStr "${matchStr}${if} ${ip} ${port}\n"
-}
-# output sss
-puts $matchStr
-#puts $expect_out(1, string)
-spawn exit
-
-send -- \001\028
-puts "192.168.0.1"
+send -- "exit\r"
+expect "logout\r"
+spawn screen -S artik -X kill
+expect eof
+puts "${str}"
