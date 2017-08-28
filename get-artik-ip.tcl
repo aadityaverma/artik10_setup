038fb3f84898df53175abec2db088a812e089db9
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-08-28 20:55:43 +0300

    Improved tests and Tcl script
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

--- get-artik-ip.tcl
+++ get-artik-ip.tcl
@@ -1,9 +1,20 @@
 #!/usr/bin/env tclsh
-#set device [lindex $argv 0]
+set default_if_name "eth0"
+set default_device "/dev/ttyUSB0"
+
+if {$argc == 1} {
+    set device [lindex $argv 0]
+    set if_name $default_if_name
+} elseif {$argc == 2} {
+    set device [lindex $argv 0]
+    set if_name [lindex $argv 1]
+} else {
+    set device $default_device
+    set if_name $default_if_name
+}
 
 package require Expect
 
-set device "/dev/ttyUSB0"
 set baud 115200
 set opts "cs8 ixoff"
 
@@ -48,10 +59,11 @@ proc connect_device {} {
     return $sid(server)
 }
 
-proc get_ip {} {
+proc get_ips {} {
     upvar screen_id screen_id
     upvar prompt prompt
     set spawn_id $screen_id
+    set str {}
 
     expect -re $prompt {
         send -- "ip -f inet addr show\r"
@@ -59,9 +71,10 @@ proc get_ip {} {
 
     expect -re $prompt {
         set matchTuples [regexp -all -inline {\d+\:\s(\w+)[^\n]*\n\s*inet\s*(\d+\.\d+\.\d+\.\d+)\/(\d+)[^\n]*} $expect_out(buffer)]
-        set str {}
         foreach {group0 group1 group2 group3} $matchTuples {
-            set str "${str}${group1} ${group2} ${group3}\n"
+            if {![regexp {^(?:lo).*} ${group1}]} {
+                set str "${str}${group1} ${group2} ${group3}\n"
+            }
         }
     }
     return $str
@@ -77,10 +90,36 @@ proc disconnect_from_device {} {
     expect eof
 }
 
+proc scratch_desirable_ip {ips_string} {
+    upvar prefferable_if if_name
+    array set ifaces {}
+    set desirable_ip ""
+    puts $prefferable_if
+
+    foreach {if_info} [split [string trim $ips_string "\n"] "\n"] {
+        set if_info_list [split $if_info]
+        dict set ifaces([lindex $if_info_list 0]) ip     [lindex $if_info_list 1]
+        dict set ifaces([lindex $if_info_list 0]) subnet [lindex $if_info_list 2]
+    }
+
+    if {[info exists ifaces($prefferable_if)]} {
+        try {
+            set desirable_ip [dict get ifaces($prefferable_if) ip]
+        } on error {msg options} {
+            exit 1
+        }
+    } else {
+        exit 1
+    }
+
+    return $desirable_ip
+}
+
 log_user 0
 
 set screen_id [connect_device]
-puts [get_ip]
+set ips [get_ips]
 disconnect_from_device
+puts [scratch_desirable_ip ips]
 
 exit 0
