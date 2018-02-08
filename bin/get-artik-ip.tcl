3a088d0f26b2444bda5387d9f5af75f07ba9f7be
Georgiy Odisharia <math.kraut.cat@gmail.com>
2018-02-08 19:08:47 +0300

    Deleted unnecesary Tcl scripts and moved main Tcl script.
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

--- bin/get-artik-ip.tcl
+++ bin/get-artik-ip.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/env tclsh
 
 if {[info exists argv0] && [file tail [info script]] eq [file tail $argv0]} {
-    lappend auto_path [file join lib tcl]
-    ::tcl::tm::path add [file join [file normalize [file dirname [info script]]] lib tcl]
+    lappend auto_path [file join .. lib tcl]
+    ::tcl::tm::path add [file join [file normalize [file dirname [info script]]] .. lib tcl]
     package require Deployer::Artik
 
     try {
