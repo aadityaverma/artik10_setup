f228b443c209c470b9178427d66d7a08c4bec1f3
Georgiy Odisharia <math.kraut.cat@gmail.com>
2018-02-08 19:00:09 +0300

    Added forgotten files
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

--- get-artik-ip.tcl
+++ get-artik-ip.tcl
@@ -5,6 +5,40 @@ if {[info exists argv0] && [file tail [info script]] eq [file tail $argv0]} {
     ::tcl::tm::path add [file join [file normalize [file dirname [info script]]] lib tcl]
     package require Deployer::Artik
 
-    ::Deployer::Artik::parse-cli-opts
-    ::Deployer::Artik::do
+    try {
+        ::Deployer::Artik::parse-cli-opts
+    } on error {reason options} {
+        switch $reason {
+            "Invalid path to device" {
+                puts "Error! Device [dict get $options device] doesn't exist. Aborting..."
+                exit 1
+            }
+        }
+    }
+
+    try {
+        ::Deployer::Artik::do
+    } on error {reason options} {
+        switch $reason {
+            "Interface has no IP" {
+                exit 2
+            }
+            "Screen terminated" {
+                exit 3
+            }
+            "Timeout" {
+                exit 4
+            }
+            "Cannot create directory!" {
+                puts "Error! Cannot create directory [dict get $options dirname]. Aborting..."
+                exit 5
+            }
+            "Cannot open file!" {
+                puts "Error! Cannot open file [dict get $options fname] for writing. Aborting..."
+                exit 5
+            }
+        }
+    }
+
+    exit 0
 }
